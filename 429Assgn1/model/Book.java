92697e074dade4d6c6c40753b7eef93b2d91b58a
davemn686 <105177998+davemn686@users.noreply.github.com>
2025-02-06 12:36:13 -0500

    Start
    Added project structure

---
+++ 429Assgn1/model/Book.java
@@ -0,0 +1,10 @@
+package model;
+
+public class Book {
+    private int bookId;
+    private String bookTitle;
+    private String author;
+    private int pubYear;
+    private String status;
+    
+}
---
+++ 429Assgn1/model/PatronCollection.java
@@ -0,0 +1,10 @@
+package model;
+
+import java.util.Vector;
+public class PatronCollection {
+    private Vector<Patron> patrons;
+
+    public PatronCollection() {
+        patrons = new Vector(); // new Vector<Patron>();
+    }
+}
