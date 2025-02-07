03ce4a38cebb8797bd4deebad971e3827aa1bb65
davemn686 <105177998+davemn686@users.noreply.github.com>
2025-02-06 20:03:23 -0500

    Implimentation
    adding configuration, adding an error log

--- 429Assgn1/model/PatronCollection.java
+++ 429Assgn1/model/PatronCollection.java
@@ -1,6 +1,22 @@
 package model;
 
+// system imports
+import java.sql.SQLException;
+import java.util.Enumeration;
+import java.util.Properties;
 import java.util.Vector;
+import javax.swing.JFrame;
+
+// project imports
+import exception.InvalidPrimaryKeyException;
+import database.*;
+
+import impresario.IView;
+
+import userinterface.View;
+import userinterface.ViewFactory;
+
+
 public class PatronCollection {
     private Vector<Patron> patrons;
 
