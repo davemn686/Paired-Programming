ddf5e74e54fe15acfd0ff2a94cd40f4cc10fad4b
davemn686 <105177998+davemn686@users.noreply.github.com>
2025-02-11 13:20:54 -0500

    Models
    fix

---
+++ ImplementationFX/model/Patron.java
@@ -0,0 +1,22 @@
+package model;
+
+// system imports
+import java.sql.SQLException;
+import java.util.Enumeration;
+import java.util.Properties;
+import java.util.Vector;
+import javax.swing.JFrame;
+
+// project imports
+import exception.InvalidPrimaryKeyException;
+import database.*;
+
+import impresario.IView;
+
+import userinterface.View;
+import userinterface.ViewFactory;
+
+
+public class Patron extends EntityBase implements IView{
+
+}
