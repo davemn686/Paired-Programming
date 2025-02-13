6aa41fa04294f10b744a38f40ea188b45ff1196f
BradonAck <backe1@brockport.edu>
2025-02-13 13:38:44 -0500

    Patron Model (no collection)

--- ImplementationFX/model/Patron.java
+++ ImplementationFX/model/Patron.java
@@ -16,8 +16,211 @@ import impresario.IView;
 import userinterface.View;
 import userinterface.ViewFactory;
 
-/***
+
 public class Patron extends EntityBase implements IView{
+    private static final String myTableName = "Patron";
+
+    protected Properties dependencies;
+
+    // GUI Components
+    private String updateStatusMessage = "";
+
+
+    // constructor for this class
+    //----------------------------------------------------------
+    public Patron(String patronId)
+            throws InvalidPrimaryKeyException
+    {
+        super(myTableName);
+
+        setDependencies();
+        String query = "SELECT * FROM " + myTableName + " WHERE (patronId = " + patronId + ")";
+
+        Vector<Properties> allDataRetrieved = getSelectQueryResult(query);
+
+        // You must get one account at least
+        if (allDataRetrieved != null)
+        {
+            int size = allDataRetrieved.size();
+
+            // There should be EXACTLY one account. More than that is an error
+            if (size != 1)
+            {
+                throw new InvalidPrimaryKeyException("Multiple patrons matching id : "
+                        + patronId + " found.");
+            }
+            else
+            {
+                // copy all the retrieved data into persistent state
+                Properties retrievedPatronData = allDataRetrieved.elementAt(0);
+                persistentState = new Properties();
+
+                Enumeration allKeys = retrievedPatronData.propertyNames();
+                while (allKeys.hasMoreElements() == true)
+                {
+                    String nextKey = (String)allKeys.nextElement();
+                    String nextValue = retrievedPatronData.getProperty(nextKey);
+                    // accountNumber = Integer.parseInt(retrievedAccountData.getProperty("accountNumber"));
+
+                    if (nextValue != null)
+                    {
+                        persistentState.setProperty(nextKey, nextValue);
+                    }
+                }
+
+            }
+        }
+        // If no account found for this user name, throw an exception
+        else
+        {
+            throw new InvalidPrimaryKeyException("No patron matching id : "
+                    + patronId + " found.");
+        }
+    }
+
+    // Can also be used to create a NEW Account (if the system it is part of
+    // allows for a new account to be set up)
+    //----------------------------------------------------------
+    public Patron(Properties props)
+    {
+        super(myTableName);
+
+        setDependencies();
+        persistentState = new Properties();
+        Enumeration allKeys = props.propertyNames();
+        while (allKeys.hasMoreElements() == true)
+        {
+            String nextKey = (String)allKeys.nextElement();
+            String nextValue = props.getProperty(nextKey);
+
+            if (nextValue != null)
+            {
+                persistentState.setProperty(nextKey, nextValue);
+            }
+        }
+    }
+
+    //-----------------------------------------------------------------------------------
+    private void setDependencies()
+    {
+        dependencies = new Properties();
+
+        myRegistry.setDependencies(dependencies);
+    }
+
+    //----------------------------------------------------------
+    public Object getState(String key)
+    {
+        if (key.equals("UpdateStatusMessage") == true)
+            return updateStatusMessage;
+
+        return persistentState.getProperty(key);
+    }
+
+    //----------------------------------------------------------------
+    public void stateChangeRequest(String key, Object value)
+    {
+
+        myRegistry.updateSubscribers(key, this);
+    }
+
+
+
+
+    /** Called via the IView relationship */
+    //----------------------------------------------------------
+    public void updateState(String key, Object value)
+    {
+        stateChangeRequest(key, value);
+    }
+
+
+
+    //-----------------------------------------------------------------------------------
+    public static int compare(Patron a, Patron b)
+    {
+        String aNum = (String)a.getState("patronId");
+        String bNum = (String)b.getState("patronId");
+
+        return aNum.compareTo(bNum);
+    }
+
+
+    //-----------------------------------------------------------------------------------
+    public void update() // save()
+    {
+        updateStateInDatabase();
+    }
+
+    //-----------------------------------------------------------------------------------
+    private void updateStateInDatabase()
+    {
+        try
+        {
+            if (persistentState.getProperty("patronId") != null)
+            {
+                // update
+                Properties whereClause = new Properties();
+                whereClause.setProperty("patronId",
+                        persistentState.getProperty("patronId"));
+                updatePersistentState(mySchema, persistentState, whereClause);
+                updateStatusMessage = "patron data for patron number : " + persistentState.getProperty("patronId") + " updated successfully in database!";
+            }
+            else
+            {
+                // insert
+                Integer patronId =
+                        insertAutoIncrementalPersistentState(mySchema, persistentState);
+                persistentState.setProperty("patronId", "" + patronId.intValue());
+                updateStatusMessage = "Patron data for new patron : " +  persistentState.getProperty("patronId")
+                        + "installed successfully in database!";
+            }
+        }
+        catch (SQLException ex)
+        {
+            updateStatusMessage = "Error in installing patron data in database!";
+        }
+        //DEBUG System.out.println("updateStateInDatabase " + updateStatusMessage);
+    }
+
+    /**
+     * This method is needed solely to enable the Account information to be displayable in a table
+     *
+     */
+    //--------------------------------------------------------------------------
+    public Vector<String> getEntryListView()
+    {
+        Vector<String> v = new Vector<String>();
+
+        v.addElement(persistentState.getProperty("patronId"));
+        v.addElement(persistentState.getProperty("address"));
+        v.addElement(persistentState.getProperty("city"));
+        v.addElement(persistentState.getProperty("stateCode"));
+        v.addElement(persistentState.getProperty("email"));
+        v.addElement(persistentState.getProperty("dateOfBirth"));
+        v.addElement(persistentState.getProperty("status"));
+        return v;
+    }
+
+    //-----------------------------------------------------------------------------------
+    protected void initializeSchema(String tableName)
+    {
+        if (mySchema == null)
+        {
+            mySchema = getSchemaInfo(tableName);
+        }
+    }
+
+    public String toString()
+    {
+        return "Name: " + persistentState.getProperty("name") + "; Address: " +
+                persistentState.getProperty("address")  + "; City: " +
+                persistentState.getProperty("city")  + "; State Code: " +
+                persistentState.getProperty("stateCode")  + "; Email: " +
+                persistentState.getProperty("email")  + "; Date Of Birth: " +
+                persistentState.getProperty("dateOfBirth")  + "; Status: " +
+                persistentState.getProperty("status") ;
+    }
+
 
 }
- **/
